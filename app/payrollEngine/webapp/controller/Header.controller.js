419a3c59507123d4146e05c5b86c2663628d6fc4
tasneemmitkees <tasneemmitkees1999@gmail.com>
2024-05-14 15:49:41 +0300

    split app to Nested  Views

---
+++ app/payrollEngine/webapp/controller/Header.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/date/UI5Date",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Core"
+],
+    function (Controller, UI5Date, JSONModel, Core) {
+        "use strict";
+
+        return Controller.extend("sp.payrollEngine.controller.Header", {
+            onInit: function () {
+            },
+        });
+    });
