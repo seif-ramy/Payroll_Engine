e8ae0902967cfedeac94a8dc82793759a0127daf
tasneemmitkees <tasneemmitkees1999@gmail.com>
2024-05-20 18:25:37 +0300

    update hompage

--- app/payrollEngine/webapp/controller/HomePage.controller.js
+++ app/payrollEngine/webapp/controller/HomePage.controller.js
@@ -2,13 +2,17 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/date/UI5Date",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Core"
+    "sap/ui/core/Core",
+    'sap/m/MessageToast'
 ],
-    function (Controller, UI5Date, JSONModel, Core) {
+    function (Controller, UI5Date, JSONModel, Core,MessageToast) {
         "use strict";
 
         return Controller.extend("sp.payrollEngine.controller.HomePage", {
             onInit: function () {
             },
+            onRunPayroll: function(){
+                MessageToast.show("Run Payroll");
+            }
         });
     });
