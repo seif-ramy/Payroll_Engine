163f680a6b1afb97d6aefe6de508e2ad49165d27
RagabAhmed <ragab.ahmedmohammed@hotmail.com>
2024-05-21 16:17:29 +0300

    HAC-36,37

--- app/payrollEngine/webapp/controller/HomePage.controller.js
+++ app/payrollEngine/webapp/controller/HomePage.controller.js
@@ -1,15 +1,47 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/date/UI5Date",
+    'sap/ui/unified/CalendarLegendItem',
+    'sap/ui/unified/DateTypeRange',
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/Core",
     'sap/m/MessageToast'
 ],
-    function (Controller, UI5Date, JSONModel, Core,MessageToast) {
+    function (Controller, UI5Date, CalendarLegendItem,DateTypeRange,JSONModel, Core,MessageToast) {
         "use strict";
 
         return Controller.extend("sp.payrollEngine.controller.HomePage", {
             onInit: function () {
+                var oCal = this.byId("calendar"),
+                oLeg = this.byId("legend"),
+                oRefDate = UI5Date.getInstance(),
+                sType;
+                for (var i = 1; i <= 10; i++) {
+                    if (i < 10) {
+                        sType = "Type0" + i;
+                    } else {
+                        sType = "Type" + i;
+                    }
+        
+                    oLeg.addItem(new CalendarLegendItem({
+                        type: sType,
+                        text: "Placeholder " + i
+                    }));
+        
+                    oRefDate.setDate(i);
+                    oCal.addSpecialDate(new DateTypeRange({
+                        startDate: UI5Date.getInstance(oRefDate),
+                        type: sType,
+                        tooltip: "Placeholder " + i
+                    }));
+        
+                    oRefDate.setDate(i + 12);
+                    oCal.addSpecialDate(new DateTypeRange({
+                        startDate: UI5Date.getInstance(oRefDate),
+                        type: sType,
+                        tooltip: "Placeholder " + i
+                    }));
+                }
             },
             onRunPayroll: function(){
                 MessageToast.show("Run Payroll");
@@ -17,5 +49,10 @@ sap.ui.define([
             onCalculatePaycheck: function(){
                 MessageToast.show("Calculate Paycheck");
             }
+        
+        
+        
+        
+        
         });
     });
