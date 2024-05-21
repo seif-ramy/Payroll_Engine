b959723edebe7fd01ad9ad7d5b415449fb11170e
MoatazAhmed7 <moatazmorsy20@gmail.com>
2024-05-21 14:08:13 +0300

    Header bar

--- app/payrollEngine/webapp/controller/PayrollPreview.controller.js
+++ app/payrollEngine/webapp/controller/PayrollPreview.controller.js
@@ -5,10 +5,24 @@ sap.ui.define([
 
     return Controller.extend("sp.payrollEngine.controller.PayrollPreview", {
         onInit: function () {},
+        onTicketButtonClick: function() {
+            // Redirect to the ticket link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
 
-        onApprove: function () {
-            // Implement the approve functionality
-            sap.m.MessageToast.show("Payroll Approved");
+        onOIPButtonClick: function() {
+            // Redirect to the OIP link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
+
+        onNotificationButtonClick: function() {
+            // Redirect to the notification link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
+
+        onProfileSettingButtonClick: function() {
+            // Redirect to the profile setting link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
         }
     });
 });
