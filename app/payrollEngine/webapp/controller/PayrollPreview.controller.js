4e8ba8c4d86a011386fc701ca23433131e2ee74b
seif-ramy <85869949+seif-ramy@users.noreply.github.com>
2024-05-21 15:43:42 +0300

    Last Commit before standup

--- app/payrollEngine/webapp/controller/PayrollPreview.controller.js
+++ app/payrollEngine/webapp/controller/PayrollPreview.controller.js
@@ -1,10 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+], function (Controller,UIComponent) {
     "use strict";
 
     return Controller.extend("sp.payrollEngine.controller.PayrollPreview", {
         onInit: function () {},
+        
+        onApprove: function() {},
+        
+        onItemSelect: function (oEvent) {
+            var sKey = oEvent.getParameter("item").getKey();
+            console.log("sKey",sKey);
+            UIComponent.getRouterFor(this).navTo(sKey);
+        },
         onTicketButtonClick: function() {
             // Redirect to the ticket link
             window.location.href = "https://rptrs.freshdesk.com/support/login";
--- app/payrollEngine/webapp/controller/PayrollProcessing.controller.js
+++ app/payrollEngine/webapp/controller/PayrollProcessing.controller.js
@@ -9,7 +9,31 @@ sap.ui.define([
 
         onNext: function () {
             // Navigate to the Preview Payroll page
+            // MessageToast.show("Approved");
             UIComponent.getRouterFor(this).navTo("PayrollPreview");
+        },
+        onItemSelect: function (oEvent) {
+            var sKey = oEvent.getParameter("item").getKey();
+            UIComponent.getRouterFor(this).navTo(sKey);
+        },
+        onTicketButtonClick: function() {
+            // Redirect to the ticket link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
+
+        onOIPButtonClick: function() {
+            // Redirect to the OIP link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
+
+        onNotificationButtonClick: function() {
+            // Redirect to the notification link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
+        },
+
+        onProfileSettingButtonClick: function() {
+            // Redirect to the profile setting link
+            window.location.href = "https://rptrs.freshdesk.com/support/login";
         }
     });
 });
